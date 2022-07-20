1fed8e77cf010e70b4c87b641a32b53ed7bc5a44
Golfsap <golf.sap@gmail.com>
2022-07-20 20:53:36 +0700

    modify repeatString function to use own for loop instead of .repeat() function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,7 +2,12 @@ const repeatString = function(string, num) {
     if (num <0) {
         return "ERROR";
     }
-    return newString = string.repeat(num);
+    let repeatedString = '';
+    //return newString = string.repeat(num);
+    for (let i = 0; i < num; i++) {
+        repeatedString += string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
