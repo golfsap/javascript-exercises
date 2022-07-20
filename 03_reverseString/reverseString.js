b018cf9270fc3cb839d4973b3b4ab2d1437e8231
Golfsap <golf.sap@gmail.com>
2022-07-20 21:20:04 +0700

    Add reverseString function using split and loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const splitStringArr = string.split('');
+    let reversedString = '';
+    for (let i = 0; i < splitStringArr.length; i++) {
+        reversedString += splitStringArr[splitStringArr.length - 1 - i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
