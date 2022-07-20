d6dc669cbdd7d1dacd7335418e208a9659bda197
Golfsap <golf.sap@gmail.com>
2022-07-20 22:36:49 +0700

    Add if statement to handle last test error

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,17 +1,23 @@
 const sumAll = function(num1, num2) {
-    let sum = 0;
-    if (Math.max(num1, num2) === num2) {
-        startInterval = num1;
-        endInterval = num2;
+    // both have to be numbers, cannot be negative
+    if ((isNaN(num1)) || isNaN(num2)) {
+        return 'ERROR';
     }
     else {
-        startInterval = num2;
-        endInterval = num1;
+        let sum = 0;
+        if (Math.max(num1, num2) === num2) {
+            startInterval = num1;
+            endInterval = num2;
+        }
+        else {
+            startInterval = num2;
+            endInterval = num1;
+        }
+        for (let i = startInterval; i <= endInterval; i++) {
+            sum += i;
+        }
+        return sum;
     }
-    for (let i = startInterval; i <= endInterval; i++) {
-        sum += i;
-    }
-    return sum;
 };
 
 // Do not edit below this line
