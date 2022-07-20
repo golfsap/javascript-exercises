2e2fe23f1505554d954231d1f63ccbc8de78f746
Golfsap <golf.sap@gmail.com>
2022-07-20 22:03:46 +0700

    Add remove function by looping through all arguments and comparing to arr

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    const args = Array.from(arguments);
+    // start index at 1 because first item contains the arr
+    for (let i = 1; i < args.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] === args[i]) {
+                arr.splice(j,1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
