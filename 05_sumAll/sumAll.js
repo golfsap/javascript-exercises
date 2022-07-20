e6db2827beb608327913022e90acba6092a14362
Golfsap <golf.sap@gmail.com>
2022-07-20 22:20:42 +0700

    Add sumAll function with ordered start and end interval

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (Math.max(num1, num2) === num2) {
+        startInterval = num1;
+        endInterval = num2;
+    }
+    else {
+        startInterval = num2;
+        endInterval = num1;
+    }
+    for (let i = startInterval; i <= endInterval; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
